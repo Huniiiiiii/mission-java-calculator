16a9d7c26ca93becd3ec56e17c27ab20fa270315
Huniiiiiii <rtg8464@gmail.com>
2025-05-13 13:03:28 +0900

    [feat] main 완성

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,16 @@
 package calculator;
 
+import calculator.Controller.CalculatorController;
+import calculator.Model.StringCalculator;
+import calculator.View.ConsoleView;
+
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        StringCalculator model = new StringCalculator();
+        ConsoleView view = new ConsoleView();
+        CalculatorController controller = new CalculatorController(model, view);
+
+        controller.run();
     }
 }
