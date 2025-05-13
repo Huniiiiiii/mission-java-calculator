7604c1f1d0f484970822345ac8fee279c625a4de
Huniiiiiii <rtg8464@gmail.com>
2025-05-13 13:01:55 +0900

    [feat] Controller 완성

--- src/main/java/calculator/Controller/CalculatorController.java
+++ src/main/java/calculator/Controller/CalculatorController.java
@@ -1,4 +1,26 @@
 package calculator.Controller;
 
+import calculator.Model.StringCalculator;
+import calculator.View.ConsoleView;
+
 public class CalculatorController {
+    private final StringCalculator calculator;
+    private final ConsoleView view;
+
+    public CalculatorController(StringCalculator calculator, ConsoleView view) {
+        this.calculator = calculator;
+        this.view = view;
+    }
+
+    public void run() {
+        String input = view.getInput();
+        try {
+            long result = calculator.add(input);
+            view.printResult(result);
+        } catch (IllegalArgumentException e) {
+            view.printError(e.getMessage());
+        } finally {
+            view.close();
+        }
+    }
 }
