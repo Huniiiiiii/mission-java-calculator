fa946edc56979dd411bd37d0681e4b3de8ca3b13
Huniiiiiii <rtg8464@gmail.com>
2025-05-13 13:01:18 +0900

    [feat]view 완성

--- src/main/java/calculator/View/ConsoleView.java
+++ src/main/java/calculator/View/ConsoleView.java
@@ -1,4 +1,23 @@
 package calculator.View;
+import java.util.Scanner;
 
 public class ConsoleView {
+    private final Scanner scanner = new Scanner(System.in);
+
+    public String getInput() {
+        System.out.println("Enter a string to calculate: ");
+        return scanner.nextLine();
+    }
+
+    public void printResult(long result) {
+        System.out.println("Result: " + result);
+    }
+
+    public void printError(String message) {
+        System.out.println("IllegalArgumentException: " + message);
+    }
+
+    public void close() {
+        scanner.close();
+    }
 }
